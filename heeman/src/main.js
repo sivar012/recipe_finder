dc5426b17d839deb6ca663ca6e4b29ff7ac4011a
sivar012 <pattemsiva368@gmail.com>
2025-12-31 15:24:43 +0530

    Delete heeman directory

--- heeman/src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var client_1 = require("react-dom/client");
-var App_tsx_1 = require("./App.tsx");
-require("./index.css");
-(0, client_1.createRoot)(document.getElementById('root')).render(<react_1.StrictMode>
-    <App_tsx_1.default />
-  </react_1.StrictMode>);
